dbec172cd3071b8dd1200435e614ab3e1101fe64
z.edd36 <z.edd@36gmail.com>
2022-06-02 00:15:32 -0500

    'inicio'

---
+++ index.js
@@ -0,0 +1,44 @@
+const httpServer = require("http").createServer();
+
+let msj = [];
+
+let users = [];
+
+let server = httpServer.listen(3001, () => {
+  console.log("escuchando 3001");
+});
+let io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", function (socket) {
+  console.log("usuario conectado", socket.id);
+  socket.emit("txtUsers", msj);
+  socket.emit("listUsers", users);
+
+  //ENVIAR TEXTO
+  socket.on("sendText", (text) => {
+    console.log("12", msj);
+    msj.push({ id: text?.id, text: text?.text });
+    io.emit("txtUsers", msj);
+
+    if (msj.length > 200) {
+      msj = [{ id: "Admin", text: "no bd maximas row" }];
+    }
+    let busqueda = users.filter((list) => list.id == text.id);
+    if (busqueda == false) {
+      users.push({ id: text?.id });
+      console.log(users);
+      io.emit("listUsers", users);
+    }
+  });
+
+  socket.on("disconnect", (text) => {
+    console.log("usuario desconectado");
+    let busqueda = users.filter((list) => list.id == text.id);
+    console.log('asd',busqueda);
+    console.log(text.id)
+  });
+});
